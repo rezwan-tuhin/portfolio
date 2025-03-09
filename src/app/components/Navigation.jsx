339bc7a75fcc2f5d5e0aba52a5122969830ec3b8
rezwan-tuhin <rezwan.tuhin@gmail.com>
2025-03-09 20:17:01 +0600

    bug fixed

--- src/app/components/Navigation.jsx
+++ src/app/components/Navigation.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navigation() {
   return (
-    <nav className='fixed top-0 left-0 w-full flex justify-center pt-1 pb-10 bg-gradient-to-b from-[#F6E9F2] to-[#FFFFFF]'>
+    <nav className='fixed top-0 left-0 w-full z-10 flex justify-center pt-1 pb-10 bg-gradient-to-b from-[#F6E9F2] to-[#FFFFFF]'>
     <ul className='flex space-x-12 mt-3 px-20 py-4 bg-gradient-to-r from-[#FCFBF5] to-[#FBFBFE] border-2 border-white rounded-4xl'>
         <li className='hover:underline'><a className='text-[20px]' href='#top'>Home</a></li>
         <li className='hover:underline'><a className='text-[20px]' href='#aboutme'>About Me</a></li>
